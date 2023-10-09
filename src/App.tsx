d1c3210cbb82d033c023f3ed48edb0c09aca7c4f
- <->
2023-10-09 17:39:28 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "@mui/material";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <Button color="primary" variant="contained">Check</Button>;
+}
+
+export default App;
